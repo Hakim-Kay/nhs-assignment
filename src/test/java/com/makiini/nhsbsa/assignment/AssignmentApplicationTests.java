16c78d2ce6c4b7cd62ea3657b67f36bc944a4fdf
Hakim Kayira <hakimkayira@192.168.1.101>
2025-07-30 11:27:50 +0100

    Initial project structure from Spring Initializr

---
+++ src/test/java/com/makiini/nhsbsa/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makiini.nhsbsa.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
