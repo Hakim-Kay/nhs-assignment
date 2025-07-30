216fd4d7b3382e3dfeb8894e3f6bcdea3b5a1b81
Hakim Kayira <hakimkayira@192.168.1.101>
2025-07-30 12:34:14 +0100

    Add  endpoint

---
+++ src/test/java/com/makiini/nhsbsa/assignment/HealthControllerTest.java
@@ -0,0 +1,25 @@
+package com.makiini.nhsbsa.assignment;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthEndpointShouldReturnOk() throws Exception {
+        mockMvc
+            .perform(get("/health"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("OK"));
+    }
+}
